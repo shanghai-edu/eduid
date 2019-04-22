c1b73c1690977ac759ba67d2cb44fac288bac0c2
Jiamin Ge <jimmy19831218@sina.com>
2019-04-22 14:13:46 +0800

    C# demo

---
+++ C#/EduIDDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduIDDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            UUIDv5.GenerateShanghaiEduID("31010519831218001X");
+        }
+    }
+}
